f8e94e6cf3dea905135ad987d2f04baacd902eea
okekefrancis112 <okeke98@gmail.com>
2025-12-08 01:14:29 +0100

    fix: redis client issue

--- server/config/redis.js
+++ server/config/redis.js
@@ -1,7 +1,14 @@
-const Redis = require("ioredis");
-const dotenv = require("dotenv");
-dotenv.config();
+// const Redis = require("ioredis");
+// const dotenv = require("dotenv");
+// dotenv.config();
 
-const redisClient = new Redis(process.env.REDIS_URL);
+// const redisClient = new Redis(process.env.REDIS_URL);
 
-module.exports = redisClient;
+// module.exports = redisClient;
+
+import { Redis } from "@upstash/redis";
+
+export const redisClient = new Redis({
+    url: process.env.UPSTASH_REDIS_REST_URL,
+    token: process.env.UPSTASH_REDIS_REST_TOKEN,
+});
